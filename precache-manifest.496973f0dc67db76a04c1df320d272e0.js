acabcfc06de6d30d7bf8ba02a062f96c0f19b76e
Bestian Tang <bestian@gmail.com>
2021-01-19 19:58:34 +0800

    deploy

---
+++ precache-manifest.496973f0dc67db76a04c1df320d272e0.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "03a02143c7dbcea46c35",
+    "url": "/fishing-10/css/about.bce72315.css"
+  },
+  {
+    "revision": "03a02143c7dbcea46c35",
+    "url": "/fishing-10/js/about.1ea5ba42.js"
+  },
+  {
+    "revision": "17029de6157b58c36534",
+    "url": "/fishing-10/css/app.e1617e78.css"
+  },
+  {
+    "revision": "17029de6157b58c36534",
+    "url": "/fishing-10/js/app.55f10101.js"
+  },
+  {
+    "revision": "32fa924f97e72c44c8d0",
+    "url": "/fishing-10/css/chunk-vendors.fe81ee62.css"
+  },
+  {
+    "revision": "32fa924f97e72c44c8d0",
+    "url": "/fishing-10/js/chunk-vendors.9ca754e4.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/fishing-10/img/flags.9c74e172.png"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/fishing-10/fonts/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/fishing-10/fonts/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/fishing-10/fonts/icons.faff9214.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/fishing-10/fonts/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/fishing-10/fonts/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/fishing-10/fonts/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/fishing-10/fonts/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/fishing-10/fonts/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/fishing-10/fonts/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/fishing-10/fonts/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/fishing-10/fonts/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/fishing-10/fonts/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/fishing-10/img/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/fishing-10/img/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/fishing-10/img/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "61bebcace44f3826e3a7b51bacd0ff98",
+    "url": "/fishing-10/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fishing-10/robots.txt"
+  }
+];
