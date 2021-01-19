4a90d45d1acf37606454dd508e9ff055f39eb129
Bestian Tang <bestian@gmail.com>
2021-01-19 19:54:02 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: '',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/fishing-10/'
+    : '/',
   pluginOptions: {
     cordovaPath: 'src-cordova',
   },
